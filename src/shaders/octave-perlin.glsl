1be8e901b9cb75c6215116ee3cf7420cac225828
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-01-28 17:56:09 -0500

    fractal noise creates mountain range, need to fix triangle strip algorithm

---
+++ src/shaders/octave-perlin.glsl
@@ -0,0 +1,20 @@
+#pragma glslify: perlin = require('glsl-noise/simplex/2d');
+
+const int OCTAVES = 6;
+const float PERSISTANCE = .5;
+
+float octavePerlin(vec2 vertex) {
+  float total = 0.;
+  float frequency = 1.;
+  float amplitude = 1.;
+  float maxValue = 0.;
+  for (int i = 0; i < OCTAVES; i++) {
+    total += amplitude * perlin(frequency * vertex / 50.);
+    maxValue += amplitude;
+    amplitude *= PERSISTANCE;
+    frequency *= 2.;
+  }
+  return total / maxValue;
+}
+
+#pragma glslify: export(octavePerlin);
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -8,15 +8,15 @@ uniform mat4 u_PerspectiveMatrix;
 varying vec3 v_PlaneVertex;
 varying vec3 v_PlaneNormal;
 
-#pragma glslify: perlin = require('glsl-noise/classic/2d');
+#pragma glslify: perlin = require('./octave-perlin.glsl');
 
 void main() {
+  float amp = 20.;
   vec2 dx = vec2(.01, 0.);
   vec2 dz = vec2(0., .01);
-  vec3 vx = vec3(a_PlaneVertex.x + .01, perlin(a_PlaneVertex + dx), a_PlaneVertex.y);
-  vec3 vz = vec3(a_PlaneVertex.x, perlin(a_PlaneVertex + dz), a_PlaneVertex.y + .01);
-
-  v_PlaneVertex = vec3(a_PlaneVertex.x, perlin(a_PlaneVertex), a_PlaneVertex.y);
+  vec3 vx = vec3(a_PlaneVertex.x + .01, amp * perlin(a_PlaneVertex + dx), a_PlaneVertex.y);
+  vec3 vz = vec3(a_PlaneVertex.x, amp * perlin(a_PlaneVertex + dz), a_PlaneVertex.y + .01);
+  v_PlaneVertex = vec3(a_PlaneVertex.x, amp * perlin(a_PlaneVertex), a_PlaneVertex.y);
   v_PlaneNormal = normalize(cross(normalize(vx - v_PlaneVertex), -1. * normalize(vz - v_PlaneVertex)));
   gl_Position = u_PerspectiveMatrix * u_ViewMatrix * vec4(v_PlaneVertex, 1.);
 }
